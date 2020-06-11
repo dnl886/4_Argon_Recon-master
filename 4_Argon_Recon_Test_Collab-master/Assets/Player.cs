dcef378c25152a94c2c50a46ee9fc7d7f06ac681
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-06-11 02:36:30 -0500

    Initial commit

---
+++ 4_Argon_Recon_Test_Collab-master/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; 
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms")] [SerializeField] float xRange = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime; //how many centermiters do i need to move in this frame
+        print(xOffsetThisFrame);
+
+        float rawXPos = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+
+        
+    }
+}
