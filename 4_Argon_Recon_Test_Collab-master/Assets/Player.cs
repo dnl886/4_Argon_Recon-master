0e20d8a55ad1f31576bc186485cad238fa856d40
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-06-11 18:18:45 -0500

    Pitch,Yaw,Roll and Changed waypt

--- 4_Argon_Recon_Test_Collab-master/Assets/Player.cs
+++ 4_Argon_Recon_Test_Collab-master/Assets/Player.cs
@@ -9,6 +9,17 @@ public class Player : MonoBehaviour
     //[Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
     [Tooltip("In ms")] [SerializeField] float xRange = 12f;
     [Tooltip("In ms")] [SerializeField] float yRange = 12f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20;
+
+    [SerializeField] float positionYawFactor = 5f;
+    //[SerializeField] float controlYawFactor = -30;
+
+    //[SerializeField] float positionRollFactor = -5f;
+    [SerializeField] float controlRollFactor = -20;
+
+    float xThrow, yThrow;
     void Start()
     {
         
@@ -23,18 +34,27 @@ public class Player : MonoBehaviour
     }
     void ProcessRotations()
     {
-        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToRotation = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToRotation;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+        
+        float roll = xThrow * controlRollFactor;
+        
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     void ProcessTranslations()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffsetThisFrame = xThrow * speed * Time.deltaTime; //how many centermiters do i need to move in this frame
         float yOffsetThisFrame = yThrow * speed * Time.deltaTime; //how many centermiters do i need to move in this frame
 
-        //print(xOffsetThisFrame);
+        Debug.Log(yOffsetThisFrame);
 
         float rawXPos = transform.localPosition.x + xOffsetThisFrame;
         float rawYPos = transform.localPosition.y + yOffsetThisFrame;
